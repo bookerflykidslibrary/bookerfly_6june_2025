f5ba1aa66203409a7ae27e64080a372c08ad6618
glittering-code <glitteringcode9@gmail.com>
2025-06-11 08:11:46 +0530

    Fixed thumbnail logic and updated Catalog UI

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,26 +9,26 @@ export default function Navbar({ user }) {
 
   return (
     <nav className="flex flex-col sm:flex-row justify-between items-center p-4 bg-blue-600 text-white space-y-2 sm:space-y-0">
-      <div className="space-x-4">
+      <div className="space-x-4" className="hover:underline">
         <Link to="/catalog">Catalog</Link>
       </div>
-      <div className="space-x-4">
+      <div className="space-x-4" className="hover:underline">
         <Link to="/my-books">My Books</Link>
       </div>
-      <div className="space-x-4">
+      <div className="space-x-4" className="hover:underline">
         <Link to="/recommendations">Recommendations</Link>
       </div>
       <div  className="space-x-4">
-        {user?.email === 'vkansal12@gmail.com' && <Link to="/admin/add-book">Add a Book</Link>}
+        {user?.email === 'vkansal12@gmail.com' && <Link to="/admin/add-book" className="hover:underline">Add a Book</Link>}
       </div>
       <div className="space-x-4">
-        {user?.email === 'vkansal12@gmail.com' && <Link to="/admin/issue-books">Issue Books</Link>}
+        {user?.email === 'vkansal12@gmail.com' && <Link to="/admin/issue-books" className="hover:underline">Issue Books</Link>}
       </div>
       <div className="space-x-4">
         {user?.email === 'vkansal12@gmail.com' && <Link to="/admin/signup" className="hover:underline">Sign Up</Link>}
       </div>
       <div  className="space-x-4">
-        {user?.email === 'vkansal12@gmail.com' && <Link to="/admin/edit-customer">View Customer</Link>}
+        {user?.email === 'vkansal12@gmail.com' && <Link to="/admin/edit-customer" className="hover:underline">View Customer</Link>}
       </div>
 
       <div className="flex items-center space-x-4">
