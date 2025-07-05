89d6b1a2db171e1003a5d3b86e4d6ba8cc211e4e
bookerflykidslibrary <bookerflykidslibrary@gmail.com>
2025-07-05 17:33:16 +0530

    updating proxy files

--- bookerfly-proxy/proxy-server.js
+++ bookerfly-proxy/proxy-server.js
@@ -5,7 +5,8 @@ const axios = require('axios');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({ origin: '*' }));
+
 
 app.get('/proxy', async (req, res) => {
     let url = req.query.url;
